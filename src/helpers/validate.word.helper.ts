1c9676a3f0a943b31d3f1c6ad62c27a4c39807db
dshagaa <lsc.edsongomez@gmail.com>
2022-07-02 16:02:38 -0500

    added: storage words locally | remove: storage library

--- src/helpers/validate.word.helper.ts
+++ src/helpers/validate.word.helper.ts
@@ -1,9 +1,17 @@
 import axios from 'axios';
-import { isEmpty, split } from 'lodash';
+import { get, isEmpty, split } from 'lodash';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const WORD_LIST_URL = 'https://gitlab.com/d2945/words/-/raw/main/words.txt';
-const WORD_LIST = [];
+let WORD_LIST = [];
+const PATH = path.join(
+  '/',
+  get(process, ['env', 'STORAGE_PATH'], '/tmp/storage'),
+);
+const FILE = 'word_list.txt';
 const GetStoredWordList = async (): Promise<string[]> => {
+  WORD_LIST = await ReadStoredWordList();
   if (isEmpty(WORD_LIST)) {
     const { data } = await axios.get(WORD_LIST_URL, {
       headers: {
@@ -13,13 +21,29 @@ const GetStoredWordList = async (): Promise<string[]> => {
         return split(data, '\n');
       },
     });
-    return data;
+    await StoreWordList(data);
+    WORD_LIST = data;
   }
   return WORD_LIST;
 };
-const StoreWordList = async (): Promise<void> => {
-  const path = '';
-};
+
+async function StoreWordList(data: string[]) {
+  fs.readdir(PATH, (err, files) => {
+    if (err) {
+      console.log('error1:');
+      fs.mkdirSync(PATH, { recursive: true });
+    }
+    fs.writeFileSync(path.join('/', PATH, FILE), JSON.stringify({ data }));
+    console.log(files);
+  });
+}
+
+async function ReadStoredWordList() {
+  const content = fs.readFileSync(path.join('/', PATH, FILE), 'utf8');
+  const { data } = JSON.parse(content);
+  return data;
+}
+
 const RenewWord = () => {
   console.log();
 };
